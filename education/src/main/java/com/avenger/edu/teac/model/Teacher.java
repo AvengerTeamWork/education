fecd964d3c4f0a243085ab746c145abd1f80d12d
1713674660 <1713674660@qq.com>
2018-08-04 09:53:01 +0800

    教师实体类

---
+++ education/src/main/java/com/avenger/edu/teac/model/Teacher.java
@@ -0,0 +1,91 @@
+package com.avenger.edu.teac.model;
+
+public class Teacher {
+
+	private int id;
+	private String name;
+	private String sex;
+	private String password;
+	private String address;
+	private String email;
+	private String phone;
+	private String pic;
+	private int courseId;
+	
+	public Teacher() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPic() {
+		return pic;
+	}
+
+	public void setPic(String pic) {
+		this.pic = pic;
+	}
+
+	public int getCourseId() {
+		return courseId;
+	}
+
+	public void setCourseId(int courseId) {
+		this.courseId = courseId;
+	}
+	
+}
--- education/src/main/java/com/avenger/edu/teac/model/q.java
+++
@@ -1,5 +0,0 @@
-package com.avenger.edu.teac.model;
-
-public class q {
-
-}
