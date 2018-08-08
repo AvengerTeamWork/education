41d2a31856ee6827ddc0b67361e7ba29ffa3ff49
1261762904 <1261762904@qq.com>
2018-08-08 16:14:02 +0800

    8.8
    修改了mapper，写了service和serviceimp

--- education/src/main/java/com/avenger/edu/stu/service/StuService.java
+++ education/src/main/java/com/avenger/edu/stu/service/StuService.java
@@ -1,5 +1,32 @@
 package com.avenger.edu.stu.service;
 
-public class StuService {
+public interface StuService {
 
+	/**
+	 * 修改密码
+	 * 
+	 * @param id
+	 * @param prePW
+	 * @param NewPw
+	 * @return
+	 */
+	public void changePW(int id, String prePW, String NewPW);
+
+	/**
+	 * 获得班级排名
+	 * 
+	 * @param id
+	 * @param time
+	 * @return
+	 */
+	public int getClaRank(int id, String time);
+
+	/**
+	 * 获得专业排名
+	 * 
+	 * @param id
+	 * @param time
+	 * @return
+	 */
+	public int getMajoRank(int id, String time);
 }
--- education/src/main/java/com/avenger/edu/stu/serviceimp/StuServiceImp.java
+++ education/src/main/java/com/avenger/edu/stu/serviceimp/StuServiceImp.java
@@ -1,5 +1,55 @@
 package com.avenger.edu.stu.serviceimp;
 
-public class StuServiceImp {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.avenger.edu.stu.mapper.StuMapper;
+import com.avenger.edu.stu.model.PoinScor;
+import com.avenger.edu.stu.service.StuService;
+
+@Service
+public class StuServiceImp implements StuService {
+
+	@Autowired
+	StuMapper stuMapper;
+
+	@Override
+	public void changePW(int id, String prePW, String NewPW) {
+		if (stuMapper.getPW(id) == prePW) {
+			stuMapper.updateStuPW(id, NewPW);
+		} else {
+			System.out.println("原密码错误");
+		}
+	}
+
+	@Override
+	public int getClaRank(int id, String time) {
+		int clasId = stuMapper.getClasByid(id);
+		int rank = 0;
+		List<PoinScor> rankList = stuMapper.getRankByclas(clasId, time);
+		for (int i = 0; i < rankList.size(); i++) {
+			if (rankList.get(i).getStuId() == id) {
+				rank = i + 1;
+				break;
+			}
+		}
+		return rank;
+	}
+
+	@Override
+	public int getMajoRank(int id, String time) {
+		int majoId = stuMapper.getMajoByid(id);
+		int rank = 0;
+		List<PoinScor> rankList = stuMapper.getRankByclas(majoId, time);
+		for (int i = 0; i < rankList.size(); i++) {
+			if (rankList.get(i).getStuId() == id) {
+				rank = i + 1;
+				break;
+			}
+		}
+		return rank;
+	}
 
 }
