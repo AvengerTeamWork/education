2d38694cc9e273d0b370aa4ee403772107561a6e
1261762904 <1261762904@qq.com>
2018-08-22 10:35:43 +0800

    。。

--- education/src/main/java/com/avenger/edu/stu/controller/StuController.java
+++ education/src/main/java/com/avenger/edu/stu/controller/StuController.java
@@ -6,8 +6,13 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+//import javax.servlet.http.Cookie;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,6 +54,7 @@ public class StuController {
 			Cookie stuId = new Cookie("id", String.valueOf(s.getStuId()));
 			resp.addCookie(stuId);
 		}
+		System.out.println(i);
 		return i;
 	}
 
